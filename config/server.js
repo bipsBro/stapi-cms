5797bab4c86c5e57652b32a387bf4dfc128c1d35
Bipin Panday <31367252+bipsBro@users.noreply.github.com>
2021-06-03 20:13:37 +0545

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '79c09c1fd28e181afe62855347e9c343'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e1f874d9-a6b3-49f4-94df-c803a06a5dff'
+};
