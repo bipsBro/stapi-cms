e08f48d04fbbd00c0af0b9eabaf204d09e443f7e
Bipin Panday <31367252+bipsBro@users.noreply.github.com>
2021-06-04 13:56:10 +0545

    setup docer file for database

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # Strapi application
 
 A quick description of your strapi application
+
+# Quick Start
+
+Clone or download this repository
+Go inside of directory, cd `client-angelhub-strapi`
+
+## setup and start Database from docker
+
+```
+docker-compose up db
+```
+
+## Setup and Run server
+
+Create `.env` file and Copy content from `.env.example`
+update the value in `.env`
+
+### Run in development mode
+
+```
+yarn develop
+```
+
+### Run in production mode
+
+Build project
+
+```
+yarn build
+```
+
+Run application
+
+```
+yarn start
+```
--- config/database.js
+++ config/database.js
@@ -4,8 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
       options: {
         useNullAsDefault: true,
